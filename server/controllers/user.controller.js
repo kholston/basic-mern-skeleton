b689b22d7aec43b37fcbc09afb4507891e461330
Ken <kennyk3n@live.com>
2021-01-18 15:38:15 -0500

    create user controller

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,11 @@
+import extend from "lodash/extend";
+import User from "../models/user.model";
+
+const create = (req, res, next) => {};
+const list = (req, res) => {};
+const userByID = (req, res, next, id) => {};
+const read = (req, res) => {};
+const update = (req, res, next) => {};
+const remove = (req, res, next) => {};
+
+export default { create, list, userByID, read, update, remove };
