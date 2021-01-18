48bb8a488d9334b009116f8e0cd3a6fc2a60c9fd
Ken Holston(Work) <git.kholston@gmail.com>
2021-01-18 00:38:11 +0000

    create template page

---
+++ template.js
@@ -0,0 +1,13 @@
+export default () => {
+  return `<!DOCTYPE html>
+		<html lang="en">
+			<head>
+				<meta charset="utf-8">
+				<title>Collab Collective</title>
+			</head>
+			<body>
+				<div id="root">Let's Collab</div>
+			</body>
+		</html>
+	`;
+};
