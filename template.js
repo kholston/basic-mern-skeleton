1ff54d400cc9e1df8db1daa895e291bf43c1ac8f
Ken Holston(Work) <git.kholston@gmail.com>
2021-01-21 20:57:02 +0000

    setup and install material ui dependencies

--- template.js
+++ template.js
@@ -4,6 +4,8 @@ export default () => {
 			<head>
 				<meta charset="utf-8">
 				<title>Collab Collective</title>
+				<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
+				<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 			</head>
 			<body>
 				<div id="root">Let's Collab</div>
