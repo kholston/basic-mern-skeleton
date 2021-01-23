8e46ea36eac5885695fc05e7cef6a0a7aa8d36f4
Ken Holston <git.kholston@gmail.com>
2021-01-23 15:33:33 -0500

    fix import webpack dev

--- server/devBundle.js
+++ server/devBundle.js
@@ -1,6 +1,6 @@
 import config from "./../config/config";
 import webpack from "webpack";
-import webpackMiddleware from "webpack-hot-middleware";
+import webpackMiddleware from "webpack-dev-middleware";
 import webpackConfig from "./../webpack.config.client";
 
 const compile = (app) => {
