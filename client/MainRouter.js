fd56b1316a314ba64681d5498ed7f7f452538887
Ken Holston <git.kholston@gmail.com>
2021-02-01 07:58:49 -0500

    fix users component, create menu component

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -6,15 +6,19 @@ import Signin from './auth/Signin';
 import Profile from './user/Profile';
 import EditProfile from './user/EditProfile';
 import PrivateRoute from './auth/PrivateRoute';
+import Menu from './core/Menu';
+import Users from './user/Users';
 
 const MainRouter = () =>{
     return ( 
         <div>
+            <Menu/>
             <Switch>
                 <PrivateRoute path="user/edit/:userId" component={EditProfile}/>
                 <Route exact path="/" component={Home}/>
                 <Route path="/signup" component={Signup}/>
                 <Route path="/signin" component={Signin} />
+                <Route path='/users' component={Users}/>
                 <Route path="/user/:userId" component={Profile}/>
             </Switch>
         </div>
