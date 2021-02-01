c05bc31f843e2834c2ee146d18af95f3ac9462a9
Ken Holston <git.kholston@gmail.com>
2021-02-01 00:43:34 -0500

    create editProfile component

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -4,11 +4,14 @@ import Home from './core/Home';
 import Signup from './user/Signup';
 import Signin from './auth/Signin';
 import Profile from './user/Profile';
+import EditProfile from './user/EditProfile';
+import PrivateRoute from './auth/PrivateRoute';
 
 const MainRouter = () =>{
     return ( 
         <div>
             <Switch>
+                <PrivateRoute path="user/edit/:userId" component={EditProfile}/>
                 <Route exact path="/" component={Home}/>
                 <Route path="/signup" component={Signup}/>
                 <Route path="/signin" component={Signin} />
